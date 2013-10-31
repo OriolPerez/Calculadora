09fc0b0db65a2c528657437adbbece4db4c2282b
OriolPerez <oriol.perez@insbaixcamp.cat>
2013-10-31 20:43:18 +0100

    Segon Commit

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -1,4 +1,7 @@
 
-public class Calculadora {
 
+public class Calculadora {
+	public static void main(String [] args) {
+		System.out.println("Taty Taty, Taty chulo, Taty Taty ven a mi... (8)");
+	}
 }
